0b655680eeaf128945ed63c91ed2d3392f0d5c72
Gustavo Bryan <devgustavob@gmail.com>
2025-09-01 12:20:56 -0300

    Funções

---
+++ Aulas/Aula2/funcoes.py
@@ -0,0 +1,16 @@
+# Funções 
+ 
+bebida1 = "café"
+bebida2 = "chá"
+bebida3 = "         suco de laranja                 "
+print("Bebida 1:", bebida1.upper()) #lower
+print("Bebida 2:", bebida2.capitalize())
+print("Bebida 3:", bebida3.title())
+print(len(bebida3)) #tamanho da string
+print(bebida3.strip()) #tira os espaços em branco
+
+nome = "Demi"
+print(f"Nome: {nome}") 
+
+texto = "Olá, mundo! \n Eu sou Demi." #Quebra de linha
+print(f"Texto: {texto}")
