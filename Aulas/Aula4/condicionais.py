a8330111aa69da8f0650649614d2dcbd9c255800
Gustavo Bryan <devgustavob@gmail.com>
2025-09-08 08:30:23 -0300

    condicionais

---
+++ Aulas/Aula4/condicionais.py
@@ -0,0 +1,9 @@
+# Condicionais
+a = 5
+b = 5
+if a > b:
+    print("A é maior que B")
+elif a == b: # elif = else if
+    print("A é igual a B")
+else:
+    print("A é menor que B")
---
+++ Aulas/Aula4/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+if idade >= 79:
+    if idade >= 65:
+        print("Idoso")
+    else:
+        print("Adulto")
+else:
+    print("Menor de idade")
